93589324083c855320147f5e0e7c2763de679134
Truls <truls.stenrud@gmail.com>
2022-12-28 20:51:36 +0100

    Try react router

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,30 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import "./App.css";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  useNavigate,
+} from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  return (
+    <RouterProvider
+      router={createBrowserRouter(
+        [
+          { path: "/", element: <PageOne /> },
+          { path: "/pageOne", element: <PageOne /> },
+          { path: "/pageTwo", element: <PageTwo /> },
+        ],
+        { basename: "/new-website-test" }
+      )}
+    />
+  );
+}
 
+const PageOne: React.FC = () => {
+  const [count, setCount] = useState(0);
+  const navigate = useNavigate();
   return (
     <div className="App">
       <div>
@@ -21,6 +41,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <button onClick={() => navigate("/pageTwo")}>Go to page two</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -30,6 +51,31 @@ function App() {
       </p>
     </div>
   );
-}
+};
+
+const PageTwo: React.FC = () => {
+  const [count, setCount] = useState(0);
+  const navigate = useNavigate();
+  return (
+    <div className="App">
+      <div>
+        <h2>Side 2</h2>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <button onClick={() => navigate("/pageOne")}>Go to page one</button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+};
 
 export default App;
