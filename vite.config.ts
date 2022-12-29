897806a1eccc3bc044eecb4c0b99b7cab5729df5
Truls <truls.stenrud@gmail.com>
2022-12-29 10:09:07 +0100

    Small changes, and add currently unnessesary vite-plugin-checker to get ready for eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: false, // vite has this included not
+      // eslint: { lintCommand: "eslint ." }, this is where we would run eslint, if or when we configure it
+      overlay: { initialIsOpen: false },
+    }),
+  ],
   base: "/new-website-test/",
   build: {
     copyPublicDir: false,
+    outDir: "build",
   },
   server: {
     port: 3000,
